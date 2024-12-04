9ac483cc819573dd261a4cdd5c6f466896d21e82
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-04 18:45:05 +0900

    3.7-3.8 completed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+
+morgan.token('person', function (req, res) { return req.body.name || req.body.number ? JSON.stringify(req.body) : ''})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
 let persons = require('./db.json')
 
