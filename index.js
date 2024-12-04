c736feb3e573bee7cc52c67b0a47da1c80043145
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-04 14:43:51 +0900

    3.2 completed

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/api/persons', (request, response) => {
   response.json(db)
 })
 
+app.get('/info', (request, response) => {
+    const number = `Phonebook has info for ${db.length} people<br/>`
+    response.send(number.concat(`${new Date(Date.now())}`))
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
