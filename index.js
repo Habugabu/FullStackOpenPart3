727a81a5dd1f5e7c63f068da69811160ecd5d6f9
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-04 15:24:49 +0900

    3.4-3.6 completed

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-const db = require('./db.json')
+let persons = require('./db.json')
 
 app.get('/api/persons', (request, response) => {
-  response.json(db)
+  response.json(persons)
 })
 
 app.get('/info', (request, response) => {
-    const number = `Phonebook has info for ${db.length} people<br/>`
+    const number = `Phonebook has info for ${persons.length} people<br/>`
     response.send(number.concat(`${new Date(Date.now())}`))
 })
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const person = db.find(person => person.id === id)
+    const person = persons.find(person => person.id === id)
     if (person) {
         response.json(person)
     }
     else {
         response.status(404).end()
     }
+})
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000).toString()
+}
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name) {
+      return response.status(400).json({ 
+        error: 'name missing' 
+      })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+    if (persons.map(person => person.name).includes(body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+  
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+  
+    response.status(204).end()
 })
 
 const PORT = 3001
