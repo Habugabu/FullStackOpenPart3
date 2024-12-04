f181dc75087d21435d8504c17a703a2c759b6054
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-04 14:47:00 +0900

    3.3 completed

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.get('/info', (request, response) => {
     response.send(number.concat(`${new Date(Date.now())}`))
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = db.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    }
+    else {
+        response.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
