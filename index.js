d2331a220d6a921525875f05b07ffcaeedb1c262
Habugabu <115887891+Habugabu@users.noreply.github.com>
2024-12-05 12:59:31 +0900

    3.15-3.18 completed

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(cors())
 morgan.token('person', function (req, res) { return req.body.name || req.body.number ? JSON.stringify(req.body) : ''})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
-let persons = require('./db.json')
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(people => {
         response.json(people)
@@ -26,7 +24,7 @@ app.get('/info', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id)
         .then(person => {
             if (person) {
@@ -36,10 +34,7 @@ app.get('/api/persons/:id', (request, response) => {
                 response.status(404).end()
             }
         })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -55,11 +50,6 @@ app.post('/api/persons', (request, response) => {
             error: 'number missing'
         })
     }
-    /*if (persons.map(person => person.name).includes(body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }*/
   
     const person = new Person ({
         name: body.name,
@@ -76,18 +66,46 @@ app.post('/api/persons', (request, response) => {
         })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    console.log(id)
-    const personEntry = persons.find(person => person.id === id)
-    if (personEntry !== undefined) {
-        persons = persons.filter(person => person.id !== id)
-        response.status(204).end()
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+      name: body.name,
+      number: body.number,
     }
-    else {
-        response.status(404).end()
-    }
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
