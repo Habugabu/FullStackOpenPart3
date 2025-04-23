b75e12b1ce569631c047e62fc8c9430d60f78b14
Habugabu <115887891+Habugabu@users.noreply.github.com>
2025-04-23 15:25:00 +0900

    properly use environment variables in workflow

--- index.js
+++ index.js
@@ -9,61 +9,75 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
 
-morgan.token("person", function (req) { return req.body.name || req.body.number ? JSON.stringify(req.body) : "";});
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :person"));
+morgan.token("person", function (req) {
+  return req.body.name || req.body.number ? JSON.stringify(req.body) : "";
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :person"
+  )
+);
 
 app.get("/api/persons", (request, response, next) => {
   Person.find({})
-    .then(people => {
+    .then((people) => {
       response.json(people);
     })
-    .catch (error => next(error));
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response, next) => {
   Person.find({})
-    .then(people => {
-      response.send(`Phonebook has info for ${people.length} people<br/>${new Date(Date.now())}`);
+    .then((people) => {
+      response.send(
+        `Phonebook has info for ${people.length} people<br/>${new Date(
+          Date.now()
+        )}`
+      );
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
-    .then(person => {
+    .then((person) => {
       if (person) {
         response.json(person);
-      }
-      else {
+      } else {
         response.status(404).end();
       }
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  const person = new Person ({
+  const person = new Person({
     name: body.name,
-    number: body.number
+    number: body.number,
   });
 
-  person.save()
-    .then(savedPerson => {
+  person
+    .save()
+    .then((savedPerson) => {
       response.json(savedPerson);
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 app.put("/api/persons/:id", (request, response, next) => {
   const { name, number } = request.body;
 
-  Person.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: "query" })
-    .then(updatedPerson => {
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: "query" }
+  )
+    .then((updatedPerson) => {
       response.json(updatedPerson);
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
@@ -71,7 +85,7 @@ app.delete("/api/persons/:id", (request, response, next) => {
     .then(() => {
       response.status(204).end();
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
